921081a3a6806a6a2313ed025022f8d295ff7e01
Justin Lee <j.lee@pokemon.com>
2018-08-13 14:30:24 -0700

    Add example unit tests

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class RomanizeTestCase(unittest.TestCase):
+
+    def test_romanize_when_input_is_3_returns_III(self):
+        pass
+
+    def test_romanize_when_input_is_4_returns_IV(self):
+        pass
+
+    def test_romanize_when_input_is_9_returns_IX(self):
+        pass
+
+    def test_romanize_when_input_is_58_returns_LVIII(self):
+        pass
+
+    def test_romanize_when_input_is_1994_returns_MCMXCIV(self):
+        pass
