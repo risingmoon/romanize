1bf797927ea8f51499b2e9fdde598573a1a17be2
Justin Lee <j.lee@pokemon.com>
2018-08-13 13:49:59 -0700

    Add notes from interview

--- romanize.py
+++ romanize.py
@@ -1,5 +1,21 @@
 #! /bin/env python3
 
 
+# step: determine numbers
+# example: 3999
+# -> 3000
+# -> 900
+# -> 90
+# -> 9
+
+# step: determine digit and place:
+# -> 3, 1000
+# -> 9, 100
+# -> 9, 10
+# -> 9, 1
+
+# step: exceptions:
+# 4, 5, 9
+
 def romanize(num):
     pass
